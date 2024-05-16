7c600d726d5acb53206e7a4237deb3bdbfa26c55
Ali Sbane <ali.sbane.dev@gmail.com>
2024-05-16 16:53:16 +0100

    Add moment.js and made the widget head dynamic

--- app.js
+++ app.js
@@ -1,30 +1,40 @@
+//
+import moment from "moment";
+
+// css
 import "./main.css";
 
-// Get today's date
-const today = new Date();
+// code
 
-// Get the day number in the month
-const dayOfMonth = today.getDate();
+document.addEventListener("DOMContentLoaded", () => {
+  // Get the HTML elements related to the widget head
+  // Where the date and time is displayed
+  const monthShortName = document.getElementById("month-short-name");
+  const dayNumber = document.getElementById("day-number");
+  const year = document.getElementById("year");
+  const dayName = document.getElementById("day-name");
+  const time = document.getElementById("time");
 
-// Get the month name with three letters
-const monthNames = [
-  "Jan",
-  "Feb",
-  "Mar",
-  "Apr",
-  "May",
-  "Jun",
-  "Jul",
-  "Aug",
-  "Sep",
-  "Oct",
-  "Nov",
-  "Dec",
-];
-const monthName = monthNames[today.getMonth()];
+  // Get the current date
+  const date = moment();
 
-// Get the year with four numbers
-const year = today.getFullYear();
+  // Fill the corresponding HTML with date data
+  monthShortName.textContent = date.format("MMM");
+  dayNumber.textContent = date.format("D");
+  year.textContent = date.format("YYYY");
+  dayName.textContent = date.format("dddd");
+  time.textContent = moment().format("HH:mm:ss");
 
-// Print the results
-console.log("Today's date:", dayOfMonth, monthName, year);
+  // Update the time
+  setInterval(() => {
+    time.textContent = moment().format("HH:mm:ss");
+  }, 1000);
+
+  // Get the ul where the tasks items are
+  const tasksUl = document.getElementById("tasks-ul");
+
+  for (let i = 0; i < tasksUl.children.length; i++) {
+    const taskLi = tasksUl.children[i];
+    console.log(taskLi);
+  }
+});
