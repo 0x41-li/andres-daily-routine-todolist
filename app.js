e40ab8a0c6b2f0ce4f42aec929c2e1d67f3a00a6
Ali Sbane <ali.sbane.dev@gmail.com>
2024-05-16 17:31:01 +0100

    Added the complete toggle functionality with some cool styles, and animation

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 //
 import moment from "moment";
+import animejs from "animejs";
 
 // css
 import "./main.css";
@@ -35,6 +36,63 @@ document.addEventListener("DOMContentLoaded", () => {
 
   for (let i = 0; i < tasksUl.children.length; i++) {
     const taskLi = tasksUl.children[i];
-    console.log(taskLi);
+
+    taskLi.addEventListener("click", () => {
+      // Get the text and line through elements
+      // plus the circle checkbox
+      const textEl = taskLi.children[1].children[0];
+      const lineThroughEl = taskLi.children[1].children[1];
+      const circleCheckboxEl = taskLi.children[0];
+      const checkInsideTheCircleCheckboxEl = circleCheckboxEl.children[0];
+
+      // Toggle between completed and not completed
+      // Using the data-completed attribute
+      if (
+        !taskLi.getAttribute("data-completed") ||
+        taskLi.getAttribute("data-completed") == "false"
+      ) {
+        taskLi.setAttribute("data-completed", "true");
+
+        // Animate the line through the task
+        animejs({
+          targets: lineThroughEl,
+          width: ["0%", "100%"],
+          opacity: [0, 1],
+          duration: 500,
+          easing: "linear",
+        });
+
+        // Change the text color
+        textEl.classList.remove("text-gray-700");
+        textEl.classList.add("text-gray-500");
+
+        // Change the circle checkbox color
+        circleCheckboxEl.classList.add("bg-green-500");
+
+        // Show the check inside the circle
+        checkInsideTheCircleCheckboxEl.classList.remove("hidden");
+      } else {
+        taskLi.setAttribute("data-completed", "false");
+
+        // Animate the line through the task
+        animejs({
+          targets: lineThroughEl,
+          width: ["100%", "0%"],
+          opacity: [1, 0],
+          duration: 500,
+          easing: "linear",
+        });
+
+        // Change the text color
+        textEl.classList.add("text-gray-700");
+        textEl.classList.remove("text-gray-500");
+
+        // Change the circle checkbox color
+        circleCheckboxEl.classList.remove("bg-green-500");
+
+        // Show the check inside the circle
+        checkInsideTheCircleCheckboxEl.classList.add("hidden");
+      }
+    });
   }
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html"],
+  content: ["./index.html", "./app.js"],
   theme: {
     extend: {},
   },
