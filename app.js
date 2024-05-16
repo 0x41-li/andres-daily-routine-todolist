950e48d36c55d2bdd105dc2bf3e18dc411791469
Ali Sbane <ali.sbane.dev@gmail.com>
2024-05-16 15:47:22 +0100

    Changed the whole design

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
 import "./main.css";
+
+// Get today's date
+const today = new Date();
+
+// Get the day number in the month
+const dayOfMonth = today.getDate();
+
+// Get the month name with three letters
+const monthNames = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+const monthName = monthNames[today.getMonth()];
+
+// Get the year with four numbers
+const year = today.getFullYear();
+
+// Print the results
+console.log("Today's date:", dayOfMonth, monthName, year);
